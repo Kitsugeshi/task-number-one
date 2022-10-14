239382846c3bf430d4b66aca05a54ada93796d9b
Видео <@->
2022-10-14 23:31:46 +0700

    Добавьте файлы проекта.

---
+++ task number one/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace task_number_one
+{
+    class ReshetoEratosfena
+    {
+        static void Main(string[] args)
+        {
+            int n, j = 0;
+            Console.WriteLine("Введите число n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = i;
+            }
+
+            a[1] = 0;
+            int m = 2;
+
+            while (m < n)
+            {
+                if (a[m] != 0)
+                    j = m * 2;
+                while (j < n)
+                {
+                    a[j] = 0;
+                    j = j + m;
+                }
+                m += 1;
+            }
+
+            foreach (int x in a)
+            {
+                if (x != 0)
+                    Console.Write(x + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
